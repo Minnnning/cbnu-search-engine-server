3dd56ff580cfc86afdf939a323d531077b4c9374
Minnnning <6alswjd6@gmail.com>
2024-05-26 15:49:21 +0900

    크롤링 1차 완려

---
+++ crawling/scripts/수의과대학/연구실험실.py
@@ -0,0 +1,20 @@
+from 수의예과 import NoticeScraper
+
+if __name__ == "__main__":
+    # 수의학과연구실험실 공지사항 설정
+    url = "https://vetmed.chungbuk.ac.kr/board/graduateschool-labs-notice.do"
+    site = "수의학과연구실험실"
+    category = "공지사항"
+    notice_list_selector = "#container > div > div.content_body > div > div.boardList > table > tbody > tr"
+    notice_contents_selector = ".boardViewContent"
+
+    scraper = NoticeScraper(url, site, category, notice_list_selector, notice_contents_selector)
+    notice_list = scraper.get_notice_list()
+    for notice in notice_list:
+        print(f"Title: {notice['title']}")
+        print(f"URL: {notice['url']}")
+        print(f"Date: {notice['date']}")
+        contents_text = scraper.get_contents_text(notice['url'])
+        print(f"Contents:\n{contents_text}")
+    scraper.close()
+    print("close")
