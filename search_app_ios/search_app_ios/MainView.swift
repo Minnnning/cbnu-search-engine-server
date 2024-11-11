e272b6209844a47ea1b34c9b96cf5e548c134a07
Minnnning <6alswjd6@gmail.com>
2024-11-11 22:47:00 +0900

    modify: 아이패드 메인뷰 수정

--- search_app_ios/search_app_ios/MainView.swift
+++ search_app_ios/search_app_ios/MainView.swift
@@ -12,90 +12,94 @@ struct MainView: View {
     private let timer = Timer.publish(every: 100, on: .main, in: .common).autoconnect()
 
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color.appBackgroundColor
-                    .ignoresSafeArea()
 
-                VStack {
-                    // 이미지 로고
-                    Image("Image")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 230, height: 180)
-                        .padding(.bottom, 20)
-
-                    // 검색창과 버튼을 담은 HStack
-                    HStack {
-                        TextField("오늘의 학식은?", text: $searchQuery, onEditingChanged: { isEditing in
-                            if isEditing && searchQuery.isEmpty {
-                                isSearchActive = false
-                                isMenuViewActive = false
+        GeometryReader { geometry in
+            NavigationStack {
+                ZStack {
+                    Color.appBackgroundColor
+                        .ignoresSafeArea()
+                    VStack {
+                        // 이미지 로고
+                        Image("Image")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 230, height: 180)
+                            .padding(.bottom, 20)
+                        
+                        // 검색창과 버튼을 담은 HStack
+                        HStack {
+                            TextField("오늘의 학식은?", text: $searchQuery, onEditingChanged: { isEditing in
+                                if isEditing && searchQuery.isEmpty {
+                                    isSearchActive = false
+                                    isMenuViewActive = false
+                                }
+                            })
+                            .padding(8)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                            Button(action: {
+                                performSearch()
+                            }) {
+                                Text("검색")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .padding(.horizontal)
+                                    .padding(.vertical, 8)
+                                    .background(Color.blue)
+                                    .cornerRadius(8)
                             }
-                        })
-                        .padding(8)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                        Button(action: {
-                            performSearch()
-                        }) {
-                            Text("검색")
+                        }
+                        .padding(.horizontal)
+                        
+                        // 실시간 검색어 표시
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("실시간 검색어")
                                 .font(.headline)
-                                .foregroundColor(.white)
-                                .padding(.horizontal)
-                                .padding(.vertical, 8)
-                                .background(Color.blue)
-                                .cornerRadius(8)
-                        }
-                    }
-                    .padding(.horizontal)
-
-                    // 실시간 검색어 표시
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("실시간 검색어")
-                            .font(.headline)
-                            .padding(.top, 20)
-
-                        ForEach(Array(searchTerms.prefix(5).enumerated()), id: \.offset) { index, term in
-                            HStack {
-                                Text("\(index + 1).")
-                                    .font(.subheadline)
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.gray)
-
-                                // 검색어 클릭 이벤트 처리 (검은색 텍스트)
-                                Text(term.token)
-                                    .font(.subheadline)
-                                    .foregroundColor(.black) // 검은색 텍스트로 설정
-                                    .padding(.leading, 5)
-                                    .onTapGesture {
-                                        searchQuery = term.token
-                                        performSearch()
-                                    }
-
-                                Spacer()
+                                .padding(.top, 20)
+                            
+                            ForEach(Array(searchTerms.prefix(5).enumerated()), id: \.offset) { index, term in
+                                HStack {
+                                    Text("\(index + 1).")
+                                        .font(.subheadline)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.gray)
+                                    
+                                    // 검색어 클릭 이벤트 처리 (검은색 텍스트)
+                                    Text(term.token)
+                                        .font(.subheadline)
+                                        .foregroundColor(.black) // 검은색 텍스트로 설정
+                                        .padding(.leading, 5)
+                                        .onTapGesture {
+                                            searchQuery = term.token
+                                            performSearch()
+                                        }
+                                    
+                                    Spacer()
+                                }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 2)
                             }
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 2)
                         }
+                        .padding(.horizontal)
+                        
+                        Spacer()
                     }
-                    .padding(.horizontal)
-
-                    Spacer()
-                }
-                .onAppear(perform: fetchSearchTerms) // 뷰가 나타날 때 실시간 검색어 가져오기
-                .onReceive(timer) { _ in
-                    fetchSearchTerms() // 타이머에 따라 일정 시간마다 API 호출
-                }
-                .navigationDestination(isPresented: $isSearchActive) {
-                    SearchResultsView(searchQuery: searchQuery)
-                }
-                .navigationDestination(isPresented: $isMenuViewActive) {
-                    MenusView()
+                    .frame(maxWidth: min(geometry.size.width, 500))
+                    .onAppear(perform: fetchSearchTerms) // 뷰가 나타날 때 실시간 검색어 가져오기
+                    .onReceive(timer) { _ in
+                        fetchSearchTerms() // 타이머에 따라 일정 시간마다 API 호출
+                    }
+                    .navigationDestination(isPresented: $isSearchActive) {
+                        SearchResultsView(searchQuery: searchQuery)
+                    }
+                    .navigationDestination(isPresented: $isMenuViewActive) {
+                        MenusView()
+                    }
                 }
             }
+                .frame(width: geometry.size.width, height: geometry.size.height)
         }
     }
 
--- search_app_ios/search_app_ios/SearchResultsView.swift
+++ search_app_ios/search_app_ios/SearchResultsView.swift
@@ -63,6 +63,7 @@ struct SearchResultsView: View {
     }()
 
     var body: some View {
+        
         ZStack {
             Color.appBackgroundColor
                 .ignoresSafeArea()
