5025b6b5140d0523218fada796851506fcce481b
minjeong kim <6alswjd6@gmail.com>
2025-02-28 14:21:20 +0900

    feat: 메뉴 sql 추가

--- DB/create_tables.sql
+++ DB/create_tables.sql
@@ -1,9 +1,20 @@
 CREATE TABLE IF NOT EXISTS notice_board (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    latitude FLOAT NULL,
+    longitude FLOAT NULL,
     url VARCHAR(255),
     title VARCHAR(255) NOT NULL,
     content TEXT,
     category VARCHAR(100),
     site VARCHAR(100),
     date DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS menus (
+    id INT AUTO_INCREMENT PRIMARY KEY,         
+    restaurantId INT NOT NULL,                 
+    restaurant_name VARCHAR(255) NOT NULL,  
+    menu TEXT NOT NULL,                        
+    time INT NOT NULL,                        
+    date DATE NOT NULL                        
 );
