88eec838b21ddc2d6237a1578765a79306c7b287
minjeong kim <6alswjd6@gmail.com>
2025-03-07 18:25:57 +0900

    modify: 학식 모듈 경로 수정

--- Crawler/학식/main.py
+++ Crawler/학식/main.py
@@ -1,13 +1,19 @@
+import sys
+import os
+import pymysql
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+
+sys.path.append(os.path.join(BASE_DIR, '학식'))
+
 from 한빛식당 import restaurant_hanbit
 from 별빛식당 import restaurant_byulbit
 from 은하수식당 import restaurant_eunhasu
-import pymysql
-
-from dotenv import load_dotenv
-import os
 
 # .env 파일 로드
-load_dotenv(dotenv_path='.env')
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
 
 # 환경 변수 설정
 hosturl = os.getenv('DB_HOST')
